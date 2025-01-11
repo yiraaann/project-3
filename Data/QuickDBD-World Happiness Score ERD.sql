b73cd5f566dd4d37a71cc65e0d096e4dfa71ecf9
Michelle Moody <mlbybee@gmail.com>
2025-01-11 14:53:36 -0600

    Add files via upload

---
+++ Data/QuickDBD-World Happiness Score ERD.sql
@@ -0,0 +1,121 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/NWHpqc
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- relationship between happiness score and energy use, gdp per capita, mobile subscriptions
+
+CREATE TABLE "happiness_score" (
+    "Country" VARCHAR(100)   NOT NULL,
+    "Happiness Score - 2012" FLOAT   NOT NULL,
+    "Happiness Score - 2013" FLOAT   NOT NULL,
+    "Happiness Score - 2015" FLOAT   NOT NULL,
+    "Happiness Score - 2016" FLOAT   NOT NULL,
+    "Happiness Score - 2017" FLOAT   NOT NULL,
+    "Happiness Score - 2018" FLOAT   NOT NULL,
+    "Happiness Score - 2019" FLOAT   NOT NULL,
+    "Happiness Score - 2020" FLOAT   NOT NULL,
+    "Happiness Score - 2021" FLOAT   NOT NULL,
+    "Happiness Score - 2022" FLOAT   NOT NULL,
+    "Happiness Score - 2023" FLOAT   NOT NULL,
+    "Happiness Score - 2024" FLOAT   NOT NULL,
+    CONSTRAINT "pk_happiness_score" PRIMARY KEY (
+        "Country"
+     )
+);
+
+CREATE TABLE "coordinates" (
+    "Latitude" FLOAT   NOT NULL,
+    "Longitude" FLOAT   NOT NULL,
+    "Country" VARCHAR(100)   NOT NULL,
+    CONSTRAINT "pk_coordinates" PRIMARY KEY (
+        "Country"
+     )
+);
+
+CREATE TABLE "energy_use" (
+    "series" VARCHAR(100)   NOT NULL,
+    "country" VARCHAR(100)   NOT NULL,
+    "code" VARCHAR(5)   NOT NULL,
+    "yr2000" FLOAT   NOT NULL,
+    "yr2001" FLOAT   NOT NULL,
+    "yr2002" FLOAT   NOT NULL,
+    "yr2003" FLOAT   NOT NULL,
+    "yr2004" FLOAT   NOT NULL,
+    "yr2005" FLOAT   NOT NULL,
+    "yr2006" FLOAT   NOT NULL,
+    "yr2007" FLOAT   NOT NULL,
+    "yr2008" FLOAT   NOT NULL,
+    "yr2009" FLOAT   NOT NULL,
+    "yr2010" FLOAT   NOT NULL,
+    "yr2011" FLOAT   NOT NULL,
+    "yr2012" FLOAT   NOT NULL,
+    "yr2013" FLOAT   NOT NULL,
+    "yr2014" FLOAT   NOT NULL,
+    "yr2015" FLOAT   NOT NULL,
+    CONSTRAINT "pk_energy_use" PRIMARY KEY (
+        "country"
+     )
+);
+
+CREATE TABLE "gdp_percap" (
+    "series" VARCHAR(100)   NOT NULL,
+    "country" VARCHAR(100)   NOT NULL,
+    "code" VARCHAR(5)   NOT NULL,
+    "yr2000" FLOAT   NOT NULL,
+    "yr2001" FLOAT   NOT NULL,
+    "yr2002" FLOAT   NOT NULL,
+    "yr2003" FLOAT   NOT NULL,
+    "yr2004" FLOAT   NOT NULL,
+    "yr2005" FLOAT   NOT NULL,
+    "yr2006" FLOAT   NOT NULL,
+    "yr2007" FLOAT   NOT NULL,
+    "yr2008" FLOAT   NOT NULL,
+    "yr2009" FLOAT   NOT NULL,
+    "yr2010" FLOAT   NOT NULL,
+    "yr2011" FLOAT   NOT NULL,
+    "yr2012" FLOAT   NOT NULL,
+    "yr2013" FLOAT   NOT NULL,
+    "yr2014" FLOAT   NOT NULL,
+    "yr2015" FLOAT   NOT NULL,
+    CONSTRAINT "pk_gdp_percap" PRIMARY KEY (
+        "country"
+     )
+);
+
+CREATE TABLE "mobile" (
+    "series" VARCHAR(100)   NOT NULL,
+    "country" VARCHAR(100)   NOT NULL,
+    "code" VARCHAR(5)   NOT NULL,
+    "yr2000" FLOAT   NOT NULL,
+    "yr2001" FLOAT   NOT NULL,
+    "yr2002" FLOAT   NOT NULL,
+    "yr2003" FLOAT   NOT NULL,
+    "yr2004" FLOAT   NOT NULL,
+    "yr2005" FLOAT   NOT NULL,
+    "yr2006" FLOAT   NOT NULL,
+    "yr2007" FLOAT   NOT NULL,
+    "yr2008" FLOAT   NOT NULL,
+    "yr2009" FLOAT   NOT NULL,
+    "yr2010" FLOAT   NOT NULL,
+    "yr2011" FLOAT   NOT NULL,
+    "yr2012" FLOAT   NOT NULL,
+    "yr2013" FLOAT   NOT NULL,
+    "yr2014" FLOAT   NOT NULL,
+    "yr2015" FLOAT   NOT NULL,
+    CONSTRAINT "pk_mobile" PRIMARY KEY (
+        "country"
+     )
+);
+
+ALTER TABLE "happiness_score" ADD CONSTRAINT "fk_happiness_score_Country" FOREIGN KEY("Country")
+REFERENCES "coordinates" ("Country");
+
+ALTER TABLE "energy_use" ADD CONSTRAINT "fk_energy_use_country" FOREIGN KEY("country")
+REFERENCES "happiness_score" ("Country");
+
+ALTER TABLE "energy_use" ADD CONSTRAINT "fk_energy_use_code" FOREIGN KEY("code")
+REFERENCES "gdp_percap" ("code");
+
+ALTER TABLE "gdp_percap" ADD CONSTRAINT "fk_gdp_percap_code" FOREIGN KEY("code")
+REFERENCES "mobile" ("code");
+
---
+++ Data/happiness_score.sql
@@ -0,0 +1,104 @@
+-- drop conditions
+DROP TABLE if exists happiness_score;
+DROP TABLE if exists coordinates;
+DROP TABLE if exists energy_use;
+DROP TABLE if exists gdp_percap;
+DROP TABLE if exists mobile;
+
+-- creating tables
+CREATE TABLE happiness_score (
+	"Country" varchar(100),
+	"Happiness Score - 2012" FLOAT,
+	"Happiness Score - 2013" FLOAT,
+	"Happiness Score - 2015" FLOAT,
+	"Happiness Score - 2016" FLOAT,
+	"Happiness Score - 2017" FLOAT,
+	"Happiness Score - 2018" FLOAT,
+	"Happiness Score - 2019" FLOAT,
+	"Happiness Score - 2020" FLOAT,
+	"Happiness Score - 2021" FLOAT,
+	"Happiness Score - 2022" FLOAT,
+	"Happiness Score - 2023" FLOAT,
+	"Happiness Score - 2024" FLOAT
+);
+
+CREATE TABLE coordinates (
+	"Latitude" double precision,
+	"Longitude" double precision,
+	"Country" varchar(100)
+);
+
+CREATE TABLE energy_use (
+	"series" varchar(100),
+	"country" varchar(100),
+	"code" varchar(5),
+	"yr2000" double precision,
+	"yr2001" double precision,
+	"yr2002" double precision,
+	"yr2003" double precision,
+	"yr2004" double precision,
+	"yr2005" double precision,
+	"yr2006" double precision,
+	"yr2007" double precision,
+	"yr2008" double precision,
+	"yr2009" double precision,
+	"yr2010" double precision,
+	"yr2011" double precision,
+	"yr2012" double precision,
+	"yr2013" double precision,
+	"yr2014" double precision,
+	"yr2015" double precision
+);
+
+CREATE TABLE gdp_percap (
+	"series" varchar(100),
+	"country" varchar(100),
+	"code" varchar(5),
+	"yr2000" double precision,
+	"yr2001" double precision,
+	"yr2002" double precision,
+	"yr2003" double precision,
+	"yr2004" double precision,
+	"yr2005" double precision,
+	"yr2006" double precision,
+	"yr2007" double precision,
+	"yr2008" double precision,
+	"yr2009" double precision,
+	"yr2010" double precision,
+	"yr2011" double precision,
+	"yr2012" double precision,
+	"yr2013" double precision,
+	"yr2014" double precision,
+	"yr2015" double precision
+);
+
+CREATE TABLE mobile (
+	"series" varchar(100),
+	"country" varchar(100),
+	"code" varchar(5),
+	"yr2000" double precision,
+	"yr2001" double precision,
+	"yr2002" double precision,
+	"yr2003" double precision,
+	"yr2004" double precision,
+	"yr2005" double precision,
+	"yr2006" double precision,
+	"yr2007" double precision,
+	"yr2008" double precision,
+	"yr2009" double precision,
+	"yr2010" double precision,
+	"yr2011" double precision,
+	"yr2012" double precision,
+	"yr2013" double precision,
+	"yr2014" double precision,
+	"yr2015" double precision
+);
+
+-- checking imports
+SELECT * FROM happiness_score;
+SELECT * FROM coordinates;
+SELECT * FROM energy_use;
+SELECT * FROM gdp_percap;
+SELECT * FROM mobile;
+
+-- additional queries
